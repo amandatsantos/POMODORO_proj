0250f51eacb4ba5240ffb921d059835277f70b25
amandatsantos <tavaresamandasantos@hotmail.com>
2021-11-07 03:45:16 -0300

    pomodoro V1

---
+++ Readme.md
@@ -0,0 +1,9 @@
+#Pomodoro 
+o objetivo é construir um programinha que ajude a contar o tempo de acordo com o metodo pomodoro.
+
+esta sendo utilizada a linguagem py
+pontos a serem melhorados:
+criar um timer para o descanso 
+ter uma maior interação com o usuario, possibilitando a escolha do tempo
+local de descanso apos o estudoo site ou programa de lazer de intervalo
+criar uma interface grafica que seja mais amigavel ao usuario 
---
+++ app.py
@@ -0,0 +1,60 @@
+#app pomodoro
+from tqdm import tqdm
+from time import time, sleep
+import os
+from math import floor
+class Pomodoro():
+
+    def __init__(self):
+        self.tempo_inicial = time ()
+        self.tempo_corrente = time()
+        self.tempo_maximo = 10
+
+    def tempo_var(self):
+        return self.tempo_corrente - self.tempo_inicial
+
+    def update(self):
+        self.tempo_corrente = time()
+
+    def display_update(self):
+        print( ' TEMPO INICIAL : ', self.tempo_maximo )
+        print( 'variação de tempo:', self.tempo_var() )
+        print('Tempo restante', self.tempo_formatado())
+        print(self.barra_progesso())
+
+    def tempo_formatado(self):
+        Xt = self.tempo_maximo - self.tempo_var()
+        MIN = floor(Xt / 60 )
+        SEC =  floor(Xt - MIN*60)
+        return '(' + str(MIN) + ':'  + str(SEC) + ')'
+
+
+    def barra_progesso(self):
+        num_chr = 1
+        data = [ ]
+        X = floor((self.tempo_maximo - self.tempo_var())/ self.tempo_maximo * num_chr)
+        Y = max(num_chr - X, 0) 
+        for i in tqdm(range(Y)):
+            data.append(i)
+            print(data) 
+
+    def mainLoop(self):
+        while True:
+            os.system("cls")
+            self.update()
+            self.display_update()
+            
+            sleep(1)
+
+            if self.tempo_var() >self.tempo_maximo:
+                print('cabo a hora de sofreeeerrr\n hora de ver u videozin :)\n ')
+                os.system('pause')
+                os.system("start https://www.youtube.com/watch?v=c3Hc7ackSe0")
+                break
+
+
+print('POMODORO 0.1\n A Técnica Pomodoro é um método de gerenciamento de tempo desenvolvido por Francesco Cirillo no final dos anos 1980.\n[1] Ele usa um cronômetro para quebrar o trabalho em intervalos, tradicionalmente de 25 minutos de duração, separados por intervalos curtos.\n Cada intervalo é conhecido como pomodoro , da palavra italiana para " tomate ", \nem homenagem ao cronômetro de cozinha em forma de tomate que Cirillo usava quando era estudante universitário.')
+os.system('pause')
+M = Pomodoro()
+M.mainLoop()
+input()
